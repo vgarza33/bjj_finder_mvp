00b4238a22ea7b1207b07613883f6287f13f0400
Woong Kim <154739147+poliwoongbjj@users.noreply.github.com>
2025-02-08 17:38:26 +0900

    Set up app.js file, modify tables in init_db.sql file, create files to setup backend API routes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var gymsRouter = require("./routes/gyms");
+var reviewsRouter = require("./routes/reviews");
 
 var app = express();
 
@@ -14,9 +15,33 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+// Serve static files from the React app
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.use("/api/gyms", gymsRouter);
+app.use("/api/reviews", reviewsRouter);
+
+// Anthing that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Send error message
+  res.status(err.status || 500);
+  res.send({ msg: "Error: " + err.message });
+});
 
 module.exports = app;
---
+++ routes/reviews.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+const db = require("../model/helper");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.send({ title: "Express" });
+});
+
+module.exports = router;
